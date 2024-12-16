6d00d35889ca4f54e4ae994bc01971201f36b908
syedhannan-33 <xyed.hannan75@outlook.com>
2024-12-17 01:19:44 +0500

    error

--- Tables/admin.py
+++ Tables/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from Tables.models import Hotel,Location,Promotions
+from Tables.models import Hotel,Location,Promotions,Room,Customer,Booking
 
 # Register your models here.
 
 admin.site.register(Hotel)
 admin.site.register(Location)
 admin.site.register(Promotions)
+admin.site.register(Room)
+admin.site.register(Customer)
+admin.site.register(Booking)
--- Tables/models.py
+++ Tables/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator , MaxValueValidator
@@ -7,48 +8,77 @@ from django.core.exceptions import ValidationError
 # Create your models here.
 
 
-class Hotel(models.Model):
-    hotel_id        = models.AutoField(primary_key=True)
-    name            = models.CharField(max_length=100)
-    stars           = models.IntegerField( validators=[MinValueValidator(1),MaxValueValidator(5)])
-    location        = models.ForeignKey('Location' , on_delete=models.CASCADE , related_name='hotels')
-    is_suspended    = models.BooleanField(default=False)
-    is_premium      = models.BooleanField(default=False)
-
-
 class Location(models.Model):
-    location_id     = models.AutoField(primary_key=True)
+    """Location Model"""
     city            = models.CharField(max_length=100)
     state           = models.CharField(max_length=100)
     country         = models.CharField(max_length=100)
 
+
+class Hotel(models.Model):
+    """Hotel Model"""
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='hotels')
+
+    name            = models.CharField(max_length=100 , unique=True)
+    stars           = models.PositiveSmallIntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+    is_suspended    = models.BooleanField(default=False)
+    is_premium      = models.BooleanField(default=False)
+
+
 class Promotions(models.Model):
-    promo_id        = models.AutoField(primary_key=True)
-    hotel_id        = models.ForeignKey('Hotel' , on_delete=models.CASCADE)
-    discount        = models.IntegerField(validators=[MinValueValidator(5) , MaxValueValidator(100)])
-    start_date = models.DateField()
-    end_date = models.DateField()
+    hotel           = models.ForeignKey(Hotel, on_delete=models.CASCADE, related_name='promotions')
+
+    discount        = models.PositiveSmallIntegerField(validators=[MinValueValidator(5) , MaxValueValidator(100)])
+    start_date      = models.DateField()
+    end_date        = models.DateField()
 
     def clean(self):
         if self.start_date > self.end_date:
             raise ValidationError("Start date cannot be after end date.")
 
-
 class Customer(models.Model):
-
+    """Customer Model"""
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customer_profile')
 
-    phone = models.CharField(max_length=20, blank=True, null=True)
-    address = models.CharField(max_length=255, blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=False, null=False,unique=True)
+    address = models.CharField(max_length=255, blank=False, null=False)
     is_blacklisted = models.BooleanField(default=False)
 
-"""
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders') 
+class Room(models.Model):
+    """Room Model"""
+    class RoomType(models.TextChoices):
+        STANDARD = 'Standard'
+        SUPERIOR = 'Superior'
+        DELUXE = 'Deluxe'
+        SUITE = 'Suite'
+        FAMILY = 'Family'
 
-    user = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders') 
-"""
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE, related_name='rooms')
+
+    room_number = models.IntegerField(default=0)
+    is_Booked = models.BooleanField(default=False)
+    type = models.CharField(max_length=100, choices=RoomType.choices)
+    price_per_night = models.DecimalField(decimal_places=2, max_digits=10)
 
 
+
+
+class Booking(models.Model):
+
+    class StatusChoices(models.TextChoices):
+        PENDING = 'Pending'
+        CONFIRMED = 'Confirmed'
+        COMPLETED = 'Completed'
+        CANCELED = 'Canceled'
+
+    """Booking Model"""
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='bookings')
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='bookings')
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE, related_name='bookings')
+
+    status = models.CharField(max_length=100 , choices=StatusChoices.choices)
+    start_date = models.DateField()
+    end_date = models.DateField()
 
 
 
