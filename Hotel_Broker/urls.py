261046eaf2fed63f58c016f7714cf10fa2ab03db
uswa-f <uswafarheen@gmail.com>
2024-12-23 13:48:35 -0800

    Text Changes

--- Hotel_Broker/urls.py
+++ Hotel_Broker/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include  # Add include here
 
+admin.site.site_header = "Hotel Admin"
+admin.site.site_title = "Hotel Admin Portal"
+admin.site.index_title = "Welcome to Hotel Admin Portal"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),  # Now include will work
